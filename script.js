4aebc3f3acade51a78fe36d868370460e468ed3b
IngridFraga <ingridfraga2017@gmail.com>
2020-10-06 05:29:03 -0300

    inclusão do sweetalert2 no código

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ function sortearNumeros() {
 function addToList(num, pos) {
   if(num.length == 2){
     if(numEsco.includes(num)){
-      alert("Numero Escolhido Anterioemente. Digite Outro numero")
+      alert2("erro", "Numero Escolhido Anterioemente. Digite Outro numero");
     } else if (parseInt(num) > 60){
-      alert("O numero digitado não pode ser maior que 60")
+      alert2("erro", "O numero digitado não pode ser maior que 60");
     } else {
       numEsco[pos-1] = num;
     }
@@ -29,7 +29,7 @@ function verificarAcertos() {
     sortearNumeros();
     let cont = 0;
     if(numEsco.length != 6){
-      alert("A quantide de numeros Escolhidos e menor que 6.\n Digite 6 numeros de 01 a 60 com duas casas decimais");
+      alert2("erro", "A quantide de numeros Escolhidos e menor que 6.\n Digite 6 numeros de 01 a 60 com duas casas decimais");
     } else {
       for (var i = 0; i < 6; i++) {
         if(numSort.includes(parseInt(numEsco[i]))){
@@ -49,4 +49,34 @@ function printNumSort() {
     li.classList.add("result");
     document.getElementById('NumSort').append(li);
   }
+}
+
+function alert2(title, text){
+  let timerInterval
+Swal.fire({
+  title: title,
+  html: text,
+  timer: 10000,
+  timerProgressBar: true,
+  willOpen: () => {
+    Swal.showLoading()
+    timerInterval = setInterval(() => {
+      const content = Swal.getContent()
+      if (content) {
+        const b = content.querySelector('b')
+        if (b) {
+          b.textContent = Swal.getTimerLeft()
+        }
+      }
+    }, 1000)
+  },
+  onClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('I was closed by the timer')
+  }
+})
 }
