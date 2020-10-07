36f9a3ccbd7b59c368defcb26b716a8fd961af25
Cheshire-dev <leonardobang123carvalho@gmail.com>
2020-10-07 16:19:27 -0300

    Inclusão do SweetAlert2

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ function sortearNumeros() {
 function addToList(num, pos) {
   if(num.length == 2){
     if(numEsco.includes(num)){
-      alert("Numero Escolhido Anterioemente. Digite Outro numero")
+      alert2("Oops...", "Numero Escolhido Anterioemente. Digite Outro numero")
     } else if (parseInt(num) > 60){
-      alert("O numero digitado não pode ser maior que 60")
+      alert2("Oops...", "O numero digitado não pode ser maior que 60")
     } else {
       numEsco[pos-1] = num;
     }
@@ -29,7 +29,7 @@ function verificarAcertos() {
     sortearNumeros();
     let cont = 0;
     if(numEsco.length != 6){
-      alert("A quantide de numeros Escolhidos e menor que 6.\n Digite 6 numeros de 01 a 60 com duas casas decimais");
+      alert2("Oops...", "A quantide de numeros Escolhidos e menor que 6.\n Digite 6 numeros de 01 a 60 com duas casas decimais");
     } else {
       for (var i = 0; i < 6; i++) {
         if(numSort.includes(parseInt(numEsco[i]))){
@@ -49,4 +49,12 @@ function printNumSort() {
     li.classList.add("result");
     document.getElementById('NumSort').append(li);
   }
+}
+
+function alert2(title, text){
+  Swal.fire({
+  icon: 'error',
+  title: title,
+  text: text
+})
 }
