26e212f01033df0afa919f8bad7fbed8e96da081
carloscacho <carlosemilio.ac@gmail.com>
2020-11-16 17:09:00 -0400

    ativar o sw

--- sw.js
+++ sw.js
@@ -2,3 +2,8 @@
 self.addEventListener('install', evt => {
   console.log('service worker está instalado');
 })
+
+// ativar o sw
+self.addEventListener('activate', evt => {
+  console.log('sw está ativado');
+})
