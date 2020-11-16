bd2bd6615de8120043f9bc77a71964e2eafc1e22
carloscacho <carlosemilio.ac@gmail.com>
2020-11-16 19:34:47 -0400

    add fetch events

--- sw.js
+++ sw.js
@@ -7,3 +7,8 @@ self.addEventListener('install', evt => {
 self.addEventListener('activate', evt => {
   console.log('sw está ativado');
 })
+
+// fetch events
+self.addEventListener('fetch', evt => {
+  console.log('fetch event');
+}) 
