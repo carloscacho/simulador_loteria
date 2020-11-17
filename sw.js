49553e4756082e10b9901bfe2b8d1b6e7197bea3
carloscacho <carlosemilio.ac@gmail.com>
2020-11-16 20:02:56 -0400

    testando novas forma para instalar

--- sw.js
+++ sw.js
@@ -12,3 +12,30 @@ self.addEventListener('activate', evt => {
 self.addEventListener('fetch', evt => {
   //console.log('fetch event', evt);
 })
+
+let deferredPrompt;
+
+self.addEventListener('beforeinstallprompt', evt => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = evt;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+});
+
+
+buttonInstall.addEventListener('click', (e) => {
+  // Hide the app provided install promotion
+  hideMyInstallPromotion();
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+  });
+});
