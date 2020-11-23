dd39835ab0ce7fcefb640ad26624f4c660831099
carloscacho <carlosemilio.ac@gmail.com>
2020-11-23 10:07:42 -0400

    save return

--- sw.js
+++ sw.js
@@ -29,7 +29,7 @@ self.addEventListener('fetch', evt => {
   //console.log('fetch event', evt);
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
-
+      return cacheRes || fetch(evt.request);
     })
-  )
+  );
 })
